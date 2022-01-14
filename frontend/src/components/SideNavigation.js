2237a17d2dca66c8f7cc75f2da596b42a693aa3b
Kevin Fu <51866248+kgfu02@users.noreply.github.com>
2022-01-13 20:32:12 -0800

    fixed css conventions

--- frontend/src/components/SideNavigation.js
+++ frontend/src/components/SideNavigation.js
@@ -6,27 +6,27 @@
  * @author Kevin Fu
  */
 import React from "react";
-
+import "./SideNavigation.css"
 /**
  * Renders the side navigation bar
  * @returns side navigation content
  */
 function SideNavigation() {
   return (
-    <div className="SideNavigation">
-      <button type="button" style={{ top: "6vh" }}>
+    <div className="side-navigation">
+      <button type="button" className="home">
         Moneta
       </button>
-      <button type="button" style={{ top: "25vh" }}>
+      <button type="button" className="database">
         Database
       </button>
-      <button type="button" style={{ top: "40vh" }}>
+      <button type="button" className="admin">
         Admin
       </button>
-      <button type="button" style={{ top: "55vh" }}>
+      <button type="button" className="settings">
         Settings
       </button>
-      <button type="button" style={{ top: "93vh" }}>
+      <button type="button" className="log-out">
         Log Out
       </button>
     </div>
