aad7cfea33f8ece4c94630cfc73a77f49ddc7e9d
Elias Fang <eliasfang@gmail.com>
2022-01-13 18:18:55 -0800

    Format with Prettier

--- frontend/src/Settings.js
+++ frontend/src/Settings.js
@@ -1,7 +1,7 @@
 /**
  * The Settings page allows users to change their password and includes three text input boxes: current password,
  * new password, and confirm new password. A save button is also included to save the user's changes.
- * 
+ *
  * @summary Settings page that allows users to change their password.
  * @author Elias Fang
  */
@@ -10,28 +10,33 @@ import React from "react";
 
 /**
  * renders the settings page
- * 
+ *
  * @returns page contents
  */
 function Settings() {
-    return (
-        <div className="Settings">
-            <header className="Settings-header">
-                <h1>
-                    Change Password
-                </h1>
-            </header>
-            <form className="Settings-form">
-                <label for="current-password">Current Password</label><br />
-                <input type="text" id="current-password" name="current-password" /><br />
-                <label for="new-password1">New Password</label><br />
-                <input type="text" id="new-password1" name="new-password1" /><br />
-                <label for="new-password2">New Password</label><br />
-                <input type="text" id="new-password2" name="new-password2" /><br /><br />
-                <input type="submit" value="Save"></input>
-            </form>
-        </div>
-    );
+  return (
+    <div className="Settings">
+      <header className="Settings-header">
+        <h1>Change Password</h1>
+      </header>
+      <form className="Settings-form">
+        <label for="current-password">Current Password</label>
+        <br />
+        <input type="text" id="current-password" name="current-password" />
+        <br />
+        <label for="new-password1">New Password</label>
+        <br />
+        <input type="text" id="new-password1" name="new-password1" />
+        <br />
+        <label for="new-password2">New Password</label>
+        <br />
+        <input type="text" id="new-password2" name="new-password2" />
+        <br />
+        <br />
+        <input type="submit" value="Save"></input>
+      </form>
+    </div>
+  );
 }
 
 export default Settings;
