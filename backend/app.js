ffb12f3703912fab38af7216cf2282c6e05fac21
pratyush1718 <pratyush.chand@outlook.com>
2022-01-15 09:58:16 -0800

    created sample user and printed tests

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,32 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const User = require("./models/Users");
 
 const app = express();
 
 connectDB();
 
-app.get("/", (req, res) => res.send("Hello world!"));
+
+const pratyush = new User({
+    fullName: 'pratyush chand',
+    email: 'haha',
+    companyID: '123',
+    password: 'hiya'
+});
+
+pratyush.save(function(err){
+    if (err) throw err
+})
+
+User.findOne({fullName: 'pratyush chand'}, function (err, user){
+    if (err) throw err;
+    console.log(user); //password field should be hashed
+    user.verifyPassword('hiya') //should return true
+});
+
+
+
+app.get("/", (req, res) => res.send("hello!"));
 
 const port = process.env.PORT || 8082;
 
