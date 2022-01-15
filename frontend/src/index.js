d00a99699d3746496e10f7a35f9b1d0a890e37b6
Elias Fang <eliasfang@gmail.com>
2022-01-15 14:23:07 -0800

    Create settings page (#24)
    * Create file and add basic form
    
    * Test render and add doc
    
    * Format with Prettier
    
    * Change label to 'Confirm New Password'

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+// import Settings from "./Settings"
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    {/* <Settings /> */}
   </React.StrictMode>,
   document.getElementById("root")
 );
