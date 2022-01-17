785eccc05c86d48eb85ed102cc2b181db61972a6
pratyush1718 <pratyush.chand@outlook.com>
2022-01-16 21:01:26 -0800

    default

--- backend/app.js
+++ backend/app.js
@@ -1,32 +1,11 @@
 const express = require("express");
 const connectDB = require("./config/db");
-const User = require("./models/Users");
 
 const app = express();
 
 connectDB();
 
-
-const pratyush = new User({
-    fullName: 'pratyush chand',
-    email: 'haha',
-    companyID: '123',
-    password: 'hiya'
-});
-
-pratyush.save(function(err){
-    if (err) throw err
-})
-
-User.findOne({fullName: 'pratyush chand'}, function (err, user){
-    if (err) throw err;
-    console.log(user); //password field should be hashed
-    user.verifyPassword('hiya') //should return true
-});
-
-
-
-app.get("/", (req, res) => res.send("hello!"));
+app.get("/", (req, res) => res.send("Hello world!"));
 
 const port = process.env.PORT || 8082;
 
