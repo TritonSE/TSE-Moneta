6123a72f02b16daecfd2c3aea992c22673cde242
ainesh01 <ainesh01@gmail.com>
2022-01-17 23:12:32 -0800

    sample group + test

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const connectDB = require("./config/db");
-
+const Group = require("./models/Groups")
 const app = express();
 
 connectDB();
+
+
+
+const test = new Group({
+    Name: 'test1',
+    Values: [{name: 'testName', type: 'testtype'}]
+});
+
+test.save(function(err){
+    if (err) throw err
+})
+
+Group.findOne({Name: 'test1'}, function (err, group){
+    if (err) throw err;
+    console.log(group); 
+});
+
+
+
 
 app.get("/", (req, res) => res.send("Hello world!"));
 
--- backend/models/Groups.js
+++ backend/models/Groups.js
@@ -1,11 +1,7 @@
-const valueSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    type: {type: String, required: true}
-});
-
+const mongoose = require('mongoose')
 const GroupSchema = new mongoose.Schema({
     Name: {type: String, required: true, index: true},
-    Values: [valueSchema]
+    Values: [{name: {type: String, required: true}, type:{type: String, required: true}}]
     
 
 },
