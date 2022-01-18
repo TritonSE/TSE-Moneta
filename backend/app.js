e572b640b9d89255484f982e6e3c9825330b3786
ainesh01 <ainesh01@gmail.com>
2022-01-17 23:15:16 -0800

    done

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,9 @@
 const express = require("express");
 const connectDB = require("./config/db");
-const Group = require("./models/Groups")
 const app = express();
 
 connectDB();
 
-
-
-const test = new Group({
-    Name: 'test1',
-    Values: [{name: 'testName', type: 'testtype'}]
-});
-
-test.save(function(err){
-    if (err) throw err
-})
-
-Group.findOne({Name: 'test1'}, function (err, group){
-    if (err) throw err;
-    console.log(group); 
-});
 
 
 
