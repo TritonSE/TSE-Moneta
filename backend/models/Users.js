e3c050321cd34ed8cbfeb951cca0fba45d0ac6f0
navidboloorian <59907006+navidboloorian@users.noreply.github.com>
2022-01-22 23:06:58 -0800

    Fixed linting (#31)

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -5,53 +5,56 @@
  * companyID (string)
  * password (string)
  * Timestamps
- * 
+ *
  * The password is hashed
- * 
+ *
  * @summary Users schema with hashed password
  * @author Pratyush Chand
  */
 
-const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
-SALT_WORK_FACTOR = 10;
-const UserSchema = new mongoose.Schema({
-    fullName: {type: String, required: true, index: true},
-    email: {type: String, required: true, index: true},
-    companyID: {type: String, required: true},
-    password: {type: String, required: true},
-    
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-},
-    {timestamps: true}
+const SALT_WORK_FACTOR = 10;
+
+const UserSchema = new mongoose.Schema(
+  {
+    fullName: { type: String, required: true, index: true },
+    email: { type: String, required: true, index: true },
+    companyID: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
 );
 
-UserSchema.pre('save', function (next){ 
-    const user = this;
-    // hashes password only if modified or new
-    if (!user.isModified('password')){return next();}
-    
-    // generate a salt
-    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-        if (err) return next(err);
-    
-        // hash the password with salt
-        bcrypt.hash(user.password, salt, function(err, hash) {
-            if (err) return next(err);
-    
-            // replace password with hashed password
-            user.password = hash;
-            next();
-        })
-    })
+UserSchema.pre("save", (next) => {
+  const user = this;
+  // hashes password only if modified or new
+  if (!user.isModified("password")) {
+    return next();
+  }
+
+  // generate a salt
+  return bcrypt.genSalt(SALT_WORK_FACTOR, (salt_err, salt) => {
+    if (salt_err) return next(salt_err);
+
+    // hash the password with salt
+    return bcrypt.hash(user.password, salt, (hash_err, hash) => {
+      if (hash_err) return next(hash_err);
+
+      // replace password with hashed password
+      user.password = hash;
+      return next();
+    });
+  });
 });
 
-UserSchema.methods.verifyPassword = function (enteredPassword){
-    bcrypt.compare(enteredPassword, this.password, function(err, matchBool){
-        if (err) throw err
-        console.log(matchBool)
-    })
-}
+UserSchema.methods.verifyPassword = (enteredPassword) =>
+  bcrypt.compare(enteredPassword, this.password, (err, matchBool) => {
+    if (err) throw err;
+    console.log(matchBool);
+  });
 
+const Users = mongoose.model("users", UserSchema);
 
-module.exports = User = mongoose.model('users', UserSchema);
+module.exports = Users;
