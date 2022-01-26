ad85de111788bea53a7d785e772fb51053bf844d
Kevin Fu <51866248+kgfu02@users.noreply.github.com>
2022-01-25 19:43:20 -0800

    completed routes, still need to test

--- backend/routes/TableDataRoutes.js
+++ backend/routes/TableDataRoutes.js
@@ -1,7 +1,40 @@
 /**
  *
- * @type
  */
 const express = require("express");
+const router = express.Router();
+const TableData = require("../models/tableData");
+const Group = require("../models/Groups");
 
-const tableDataRouter = express.Router();
+router.post("/addRow", (req, res) => {
+  const tableData = new TableData({
+    group: req.body.group,
+    data: req.body.data,
+  });
+  console.log("a");
+  tableData.save();
+  res.send("Posted TableData!");
+});
+
+router.put("/editRow/:id", (req, res) => {
+  TableData.updateOne({ _id: req.params.id }, req.body);
+});
+
+router.get("/getAllRows/:id", (req, res) => {
+  TableData.find({
+    _id: req.params.id,
+  })
+    .exec()
+    .then((data) => {
+      res.send(data);
+      console.log(data);
+    });
+});
+
+router.get("/getAllRows", (req, res) => {
+  TableData.find({})
+    .exec()
+    .then((data) => res.send(data));
+});
+
+module.exports = router;
