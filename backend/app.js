feead2ed9de839143b0b2c08da0fff2a6170595c
pratyush1718 <pratyush.chand@outlook.com>
2022-01-26 14:57:15 -0800

    Vaildation error

--- backend/app.js
+++ backend/app.js
@@ -11,4 +11,7 @@ app.get("/", (req, res) => res.send("Hello world!"));
 
 const port = process.env.PORT || 8082;
 
+const routes = require('./routes/userRoutes')
+app.use('/routes', routes)
+
 app.listen(port, () => console.log(`Server running on port ${port}`));
