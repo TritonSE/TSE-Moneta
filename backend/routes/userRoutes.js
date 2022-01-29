736bb7447bfbf7a5e5eaaefc3950ce6b28fc87a5
pratyush1718 <pratyush.chand@outlook.com>
2022-01-29 14:16:51 -0800

    Updated routes and Schema

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,22 @@
+/**
+ * API Routes for Users Schema
+ * POST (/addUser)
+ * DELETE (/deleteUser/:id)
+ * PUT (/editUser/:id)
+ * GET (/getUser/:id)
+ * GET (/getAllUsers)
+ *
+ *
+ * @summary Users routes for add, edit, delete, and get
+ * @author Pratyush Chand
+ */
+
 const express = require("express");
+const User = require("../models/Users");
 
 const router = express.Router();
 
-// Load Users model
 const Users = require("../models/Users");
-
-// /addUser
 
 router.post("/addUser", async (req, res) => {
   try {
@@ -28,7 +39,7 @@ router.post("/addUser", async (req, res) => {
 
     if (addUser) {
       return res.status(200).json({
-        Message: "Success",
+        addUser,
       });
     }
 
@@ -41,36 +52,113 @@ router.post("/addUser", async (req, res) => {
   }
 });
 
-// remove route to be added
+router.delete("/deleteUser/:id", async (req, res) => {
+  try {
+    const userExists = await User.exists({ _id: req.params.id });
 
-router.put("/editUser/:id", (req, res) => {
-  Users.findByIdAndUpdate(req.params.id, req.body)
-    .then((user) => {
-      console.log(user);
-    })
-    .catch((err) => {
-      res.statusCode(400).json({ error: err });
+    if (!userExists) {
+      return res.status(400).json({
+        msg: "Such a user does not exist!",
+      });
+    }
+
+    const deletedUser = await Users.findByIdAndDelete(req.params.id);
+
+    if (deletedUser) {
+      return res.status(200).json({
+        msg: "success",
+      });
+    }
+
+    return res.status(400).json({
+      Error: "Error",
     });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: err });
+  }
 });
 
-router.get("/getUser/:id", (req, res) => {
-  Users.findById(req.params.id)
-    .then((user) => {
-      console.log(user);
-    })
-    .catch((err) => {
-      res.statusCode(400).json({ error: err });
+router.put("/editUser/:id", async (req, res) => {
+  try {
+    const userExists = await User.exists({ _id: req.params.id });
+
+    if (!userExists) {
+      return res.status(400).json({
+        msg: "Such a user does not exist!",
+      });
+    }
+
+    const { fullName, email, companyID, password } = req.body;
+
+    const user = {
+      fullName,
+      email,
+      companyID,
+      password,
+    };
+
+    const editUser = await Users.findOneAndUpdate(req.params.id, user);
+
+    if (editUser) {
+      return res.status(200).json({
+        msg: "successfully edited!",
+      });
+    }
+
+    return res.status(500).json({
+      Error: "Error",
     });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: err });
+  }
 });
 
-router.get("/getAllUsers", (req, res) => {
-  Users.find()
-    .then((users) => {
-      console.log(users);
-    })
-    .catch((err) => {
-      res.statusCode(400).json({ error: err });
+router.get("/getUser/:id", async (req, res) => {
+  try {
+    const userExists = await User.exists({ _id: req.params.id });
+
+    if (!userExists) {
+      return res.status(400).json({
+        msg: "_id does not exist!",
+      });
+    }
+
+    const getUser = await Users.findById(req.params.id);
+
+    if (getUser) {
+      return res.status(200).json({
+        getUser,
+      });
+    }
+
+    return res.status(500).json({
+      Error: "Error",
     });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: err });
+  }
+});
+
+router.get("/getAllUsers", async (req, res) => {
+  try {
+    const allUsers = await Users.find();
+
+    if (allUsers) {
+      return res.status(200).json({
+        allUsers,
+      });
+    }
+
+    return res.status(500).json({
+      Error: "Error",
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: err });
+  }
 });
 
 module.exports = router;
