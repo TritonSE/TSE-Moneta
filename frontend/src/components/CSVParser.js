a6d85f118caaac74cbfc0ad83ca7714805e1a218
Elias Fang <eliasfang@gmail.com>
2022-02-01 10:24:53 -0800

    Merge main into branch

---
+++ frontend/src/components/CSVParser.js
@@ -0,0 +1,22 @@
+/**
+ * Allows users to upload CSV files that get parsed into JSON objects
+ * to populate the dashboard table.
+ *
+ * @summary CSV parser for dashboard table
+ * @author Elias Fang
+ */
+
+import React from 'react';
+import { usePapaParse } from 'react-papaparse';
+
+// TODO: import css
+
+/**
+ * Renders the CSV parser
+ * @returns CSV parser content
+ */
+function CSVParser() {
+
+}
+
+export default CSVParser;
