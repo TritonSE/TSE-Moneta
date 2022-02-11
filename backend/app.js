111b1efc8abe45be7e0791ee8d0c0c8d4625c594
Kevin Fu <51866248+kgfu02@users.noreply.github.com>
2022-02-10 22:36:51 -0800

    linked parser to backed

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,11 @@ const userRoutes = require("./routes/userRoutes");
 const companyRoutes = require("./routes/companiesRoutes");
 
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", TableDataRoutes);
 
 connectDB();
@@ -14,9 +19,14 @@ app.get("/", (req, res) => res.send("Hello world!"));
 
 const port = process.env.PORT || 8082;
 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/userRoutes", userRoutes);
 app.use("/companiesRoutes", companyRoutes);
+
+const routes = require("./routes/userRoutes");
+app.use("/routes", routes);
+
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
