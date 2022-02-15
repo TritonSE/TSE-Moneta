75d86e7431f821503818cf60b52bd1583e77bb65
Alex Zhang <alexzhang1618@gmail.com>
2022-02-15 12:24:44 -0800

    Positioning and icon placement for side navigation

--- frontend/src/components/SideNavigation.js
+++ frontend/src/components/SideNavigation.js
@@ -7,6 +7,11 @@
  */
 import React from "react";
 import { Link } from "react-router-dom";
+import Logo from "../images/Logo.svg";
+import DatabaseIcon from "../images/DatabaseIcon";
+import AdminIcon from "../images/AdminIcon";
+import SettingsIcon from "../images/SettingsIcon";
+import LogOut from "../images/LogOut.svg";
 import "../css/SideNavigation.css";
 /**
  * Renders the side navigation bar
@@ -16,24 +21,35 @@ function SideNavigation() {
   return (
     <div className="side-navigation">
       <Link to="/">
-        <button type="button" className="home">
+        <button type="button" className="sidenav-home">
+          <img src={Logo} className="sidenav-logo" alt="Moneta" />
           Moneta
         </button>
       </Link>
-      <button type="button" className="database">
-        Database
-      </button>
+
+      <Link to="/">
+        <button type="button" className="sidenav-button database">
+          <DatabaseIcon className="sidenav-icon database" />
+        </button>
+        <p className="sidenav-description database">Database</p>
+      </Link>
+
       <Link to="/admin">
-        <button type="button" className="admin">
-          Admin
+        <button type="button" className="sidenav-button admin">
+          <AdminIcon className="sidenav-icon admin" />
         </button>
+        <p className="sidenav-description admin">Admin</p>
       </Link>
+
       <Link to="/settings">
-        <button type="button" className="settings">
-          Settings
+        <button type="button" className="sidenav-button settings">
+          <SettingsIcon className="sidenav-icon settings" />
         </button>
+        <p className="sidenav-description settings">Settings</p>
       </Link>
-      <button type="button" className="log-out">
+
+      <button type="button" className="sidenav-logout">
+        <img src={LogOut} className="sidenav-icon-logout" alt="Log Out" />
         Log Out
       </button>
     </div>
