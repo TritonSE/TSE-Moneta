6cff0709c392dbc18cb57d9f04272d73e6af2f05
Navid Boloorian <navidboloorian0@gmail.com>
2022-02-21 16:16:04 -0800

    Added functionality

---
+++ backend/models/Counter.js
@@ -0,0 +1,24 @@
+/**
+ * Counter schema is used to assign readable, sequential ids for data entries
+ *
+ * @summary Counter schema
+ * @author Navid Boloorian
+ */
+
+const mongoose = require("mongoose");
+
+const CounterSchema = new mongoose.Schema(
+  {
+    fieldName: {type: String, required: true},
+    idVal: {type: Number, default: 0}
+  },
+);
+
+const Counter = mongoose.model("counters", CounterSchema);
+
+if(Counter.count({'fieldName': 'GroupId'}) == 0) {
+    const counter = new Counter({'fieldName': 'GroupId'});
+    counter.save();
+}
+
+module.exports = Counter;
