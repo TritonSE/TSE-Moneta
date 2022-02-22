7c0249851968cb3c61d2c06282b3d88bfb809923
Navid Boloorian <navidboloorian0@gmail.com>
2022-02-21 16:16:04 -0800

    Added functionality

--- backend/models/Groups.js
+++ backend/models/Groups.js
@@ -9,14 +9,22 @@
  */
 
 const mongoose = require("mongoose");
+const Counter = require("../models/Counter");
 
 const GroupSchema = new mongoose.Schema(
   {
     Name: { type: String, required: true, index: true },
     Values: [{ name: { type: String, required: true }, type: { type: String, required: true } }],
+    GroupId: { type: Number, required: true, index: true, default: 0}
   },
   { timestamps: true }
 );
+
+GroupSchema.pre('save', async function() {
+    const counter = await Counter.find({'fieldName': 'GroupId'});
+    this.GroupId = counter[0].idVal;
+    await Counter.updateOne({'fieldName': 'GroupId'}, {$inc: {'idVal': 1}});
+})
 
 const Group = mongoose.model("groups", GroupSchema);
 
--- backend/models/TableData.js
+++ backend/models/TableData.js
@@ -8,12 +8,10 @@
 
 const mongoose = require("mongoose");
 
-const ObjectId = mongoose.Schema.Types.ObjectId;
-
 const tableDataSchema = mongoose.Schema(
   {
     group: {
-      type: ObjectId,
+      type: Number,
       ref: "Group",
       required: true,
     },
