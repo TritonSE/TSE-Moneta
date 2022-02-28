035b48b0008bb2b74e49882357c826a461726647
Navid Boloorian <navidboloorian0@gmail.com>
2022-02-28 12:07:19 -0800

    Tweaks to pull from remote db

--- backend/app.js
+++ backend/app.js
@@ -8,21 +8,18 @@ const companyRoutes = require("./routes/companiesRoutes");
 
 const app = express();
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/", TableDataRoutes);
-
 connectDB();
 
 app.get("/", (req, res) => res.send("Hello world!"));
 
 const port = process.env.PORT || 8082;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/userRoutes", userRoutes);
 app.use("/companiesRoutes", companyRoutes);
+app.use("/", TableDataRoutes);
 
 const routes = require("./routes/userRoutes");
 
