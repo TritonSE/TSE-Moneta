6714cdcbf41e540ada63b4db216b6f8396abbc06
Alex Zhang <alexzhang1618@gmail.com>
2022-02-27 21:20:51 -0800

    implement basic search route, make TableDataRoutes more restful, fix linting

--- backend/app.js
+++ backend/app.js
@@ -18,5 +18,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/userRoutes", userRoutes);
 app.use("/companiesRoutes", companyRoutes);
+app.use("/TableDataRoutes", TableDataRoutes);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
--- backend/routes/TableDataRoutes.js
+++ backend/routes/TableDataRoutes.js
@@ -27,13 +27,13 @@ async function isValidGroup(id, res) {
 }
 
 /**
- * "/addRow" - inserts new TableData
+ * POST "/row" - inserts new TableData
  * Duplicate data is if all columns are equivalent and they are in the same group return 409
  * If required values are missing return 500
  * If neither of the above is true return 200
  * Return 500 in a catch if something else goes wrong
  */
-router.post("/addRow", [body("group").exists(), body("data").exists()], async (req, res) => {
+router.post("/row", [body("group").exists(), body("data").exists()], async (req, res) => {
   try {
     // check if all required fields are present
     const errors = validationResult(req);
@@ -70,13 +70,13 @@ router.post("/addRow", [body("group").exists(), body("data").exists()], async (r
 });
 
 /**
- * "/editRow:id" - edits TableData
+ * PUT "/rows/:id" - edits TableData
  * If edit results in duplicate data return 409
  * If id does not exist return 500
  * If neither of the above is true return 200
  * Return 500 in a catch if something else goes wrong
  */
-router.put("/editRow/:id", async (req, res) => {
+router.put("/rows/:id", async (req, res) => {
   try {
     if (!(await isValidGroup(req.body.group, res))) {
       return;
@@ -100,12 +100,12 @@ router.put("/editRow/:id", async (req, res) => {
 });
 
 /**
- * "/getRow/:id" - gets TableData by id
+ * GET "/rows/:id" - gets TableData by id
  * If id does not exist return 500
  * If it does return 200
  * Return 500 in a catch if something else goes wrong
  */
-router.get("/getAllRows/:id", (req, res) => {
+router.get("/rows/:id", (req, res) => {
   try {
     TableData.find({ _id: req.params.id })
       .then((data) => {
@@ -125,11 +125,36 @@ router.get("/getAllRows/:id", (req, res) => {
 });
 
 /**
- * "/getAllRows" - gets all TableDatas
+ * GET "/rows" - gets all TableDatas
  * Return 200
  */
-router.get("/getAllRows", (req, res) => {
+router.get("/rows", (req, res) => {
   TableData.find({}).then((data) => res.json(data));
+});
+
+/**
+ * GET "/groups" - gets all Groups
+ * Returns 200
+ */
+router.get("/groups", (req, res) => {
+  Group.find({}).then((data) => res.json(data));
+});
+
+router.post("/search", [body("group").exists(), body("search").exists()], async (req, res) => {
+  if (!(await isValidGroup(req.body.group, res))) {
+    return;
+  }
+  const tableData = await TableData.findOne({group: req.body.group});
+  const ret = []
+  for (const entry of tableData.data) {
+    for (const [_field, value] of Object.entries(entry)) {
+      if (value.toLowerCase().includes(req.body.search.toLowerCase())) {
+        ret.push(entry);
+        break;
+      }
+    }
+  }
+  res.json(ret);
 });
 
 module.exports = router;
