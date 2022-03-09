dc130c9593a1bc92199b0279581c6127983d0b9b
Elias Fang <eliasfang@gmail.com>
2022-03-08 22:58:32 -0800

    Merge main and test function

--- backend/models/Counter.js
+++ backend/models/Counter.js
@@ -14,11 +14,7 @@ const CounterSchema = new mongoose.Schema({
 
 const Counter = mongoose.model("counters", CounterSchema);
 
-<<<<<<< HEAD
-if (Counter.count({ fieldName: "GroupId" }) == 0) {
-=======
 if (Counter.count({ fieldName: "GroupId" }) === 0) {
->>>>>>> 5564a30a841af5cae79136def675340780de8ea2
   const counter = new Counter({ fieldName: "GroupId" });
   counter.save();
 }
