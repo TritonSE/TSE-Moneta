de8ce6b4394792820ae77c53e1b0ef4b96dbe4c4
Kevin Fu <51866248+kgfu02@users.noreply.github.com>
2022-03-09 20:43:04 -0800

    Form verifies login info with /login route.

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,32 +1,58 @@
-import React, { useState } from "react";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import "../css/Login.css";
 
-const handleSubmit = async (event) => {
-  event.preventDefault();
-  console.log(event.target.username.value);
-  console.log(event.target.password.value);
-  console.log("submit");
-};
-
+/**
+ * Renders login page
+ * @author Kevin Fu
+ */
 function Login() {
+  const navigate = useNavigate();
+
+  /**
+   * Posts data to server and handles response
+   */
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = { email: event.target.email.value, password: event.target.password.value };
+    const result = await fetch("http://localhost:8082/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+      mode: "cors",
+    });
+    if (result.status === 200) {
+      console.log("Logged in!");
+      navigate("/");
+    } else if (result.status === 404) {
+      console.log("Email not found!");
+    } else if (result.status === 400) {
+      console.log("Invalid Password!");
+    }
+  };
+
   return (
-    <>
-      <div className="login-form">
-        <form onSubmit={handleSubmit}>
-          <div className="login-input-container">
-            <label>Username </label>
-            <input type="text" name="username" required />
-          </div>
-          <div className="login-input-container">
-            <label>Password </label>
+    <div className="login-form">
+      <form onSubmit={handleSubmit}>
+        <div className="login-input-container">
+          <label htmlFor="login-email">
+            Email
+            <input type="text" name="email" required />
+          </label>
+        </div>
+        <div className="login-input-container">
+          <label htmlFor="login-password">
+            Password
             <input type="password" name="password" required />
-          </div>
-          <div className="login-submit-button-container">
-            <input type="submit" />
-          </div>
-        </form>
-      </div>
-    </>
+          </label>
+        </div>
+        <div className="login-submit-button-container">
+          <input type="submit" />
+        </div>
+      </form>
+    </div>
   );
 }
 
