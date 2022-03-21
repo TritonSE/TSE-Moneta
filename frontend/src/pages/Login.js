5d8fe91333623c5bcf37573781edc7b1e026944a
Navid Boloorian <navidboloorian0@gmail.com>
2022-03-21 16:47:15 -0700

    Login frontend

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,60 +1,102 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import emailjs from "emailjs-com";
+import { Snackbar, Alert } from "@mui/material";
+import Logo from "../images/Logo.svg";
+import Back from "../images/BackButton.svg";
+
+import "../css/Account.css";
 import "../css/Login.css";
 
-/**
- * Renders login page
- * @author Kevin Fu
- * @author Navid Boloorian
- */
-function Login() {
-  const navigate = useNavigate();
-
-  /**
-   * Posts data to server and handles response
-   */
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const data = { email: event.target.email.value, password: event.target.password.value };
-    const result = await fetch("http://localhost:8082/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-      mode: "cors",
+export default function Login() {
+    const registerForm = React.useRef();
+    const [registered, setRegistered] = React.useState(false);
+    const [snackbar, setSnackbar] = React.useState({
+      open: false,
+      message: "",
+      severity: "",
     });
-    if (result.status === 200) {
-      console.log("Logged in!");
-      navigate("/");
-    } else if (result.status === 404) {
-      console.log("Email not found!");
-    } else if (result.status === 400) {
-      console.log("Invalid Password!");
+
+    const handleSnackClose = () => {
+        setSnackbar({
+          open: false,
+          message: "",
+          severity: "",
+        });
+    };
+
+    const formCheck = async (e) => {
+        e.preventDefault();
+
+        const email = registerForm.current[0].value;
+        const password = registerForm.current[1].value;
+
+        const response = await fetch("http://localhost:8082/organizations", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ Name: name, Email: email }),
+        });
+
+        const json = await response.json();
+
+        if(response.ok) {
+            sendEmail(e);
+            setRegistered(true);
+        }
+        else {
+            if(response.status == 409)
+                setSnackbar({      
+                    open: true,
+                    message: json.msg,
+                    severity: "error",
+                })
+            else 
+                setSnackbar({      
+                    open: true,
+                    message: "Server error. Try again later",
+                    severity: "error",
+                })
+        }
     }
-  };
 
-  return (
-    <div className="login-form">
-      <form onSubmit={handleSubmit}>
-        <div className="login-input-container">
-          <label htmlFor="login-email">
-            Email
-            <input type="text" name="email" required />
-          </label>
+    return (
+        <div className="account-wrapper">
+            <div className="background-half">
+            </div>
+            <div className="form-half">
+                <div>
+                    {registered && <div id="button-container" onClick={()=>setRegistered(false)}><img src={Back} /><p>Back</p></div>}
+                    <div className="account-logo" >
+                        <img src={Logo}/>
+                        <h1>Moneta</h1>
+                    </div>
+                    <div className="account-form">
+                        <h2>Log Into Your Account</h2>
+                        <p>Don't have an account? <a href="#">Sign Up</a></p>
+                        <form ref={registerForm} onSubmit={formCheck}>
+                            <label for="nonprofit-email">Email address</label><br />
+                            <input name="nonprofit-email" id="nonprofit-email" type="email"placeholder="Enter Email" /><br />
+                            <label for="nonprofit-password">Name of nonprofit</label><br />
+                            <input name="nonprofit-password" id="nonprofit-password" type="password" placeholder="Enter Password" /><br />
+                            <input type="submit" id="account-submit" value="Continue" />
+                        </form>  
+                    </div>
+                </div>
+            </div>
+            <Snackbar
+                open={snackbar.open}
+                autoHideDuration={6000}
+                onClose={handleSnackClose}
+                anchorOrigin={{ horizontal: "center", vertical: "bottom" }}
+            >
+                <Alert
+                    onClose={handleSnackClose}
+                    severity={snackbar.severity}
+                    sx={{ width: "100%" }}
+                    variant="filled"
+                >
+                    {snackbar.message}
+                </Alert>
+            </Snackbar>
         </div>
-        <div className="login-input-container">
-          <label htmlFor="login-password">
-            Password
-            <input type="password" name="password" required />
-          </label>
-        </div>
-        <div className="login-submit-button-container">
-          <input type="submit" />
-        </div>
-      </form>
-    </div>
-  );
+    )
 }
-
-export default Login;
