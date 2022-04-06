75963cbf1ee6fbfa77336bd6eb136f6549e86a4a
William Wu <dev.wwu@gmail.com>
2022-04-06 00:17:07 -0700

    Initial email util implementation

---
+++ utils/emails.js
@@ -0,0 +1,39 @@
+/**
+ * Collection of functions for automatically sending emails with different
+ * templates. Email sender address and authentication information is stored in
+ * the .env file.
+ * @summary Automatic email sending utilities
+ * @author William Wu
+ */
+
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport(process.env.EMAIL_SENDER_CONNECTION_URL, {
+  from: "tsemoneta@gmail.com",
+});
+
+export const sendOrganizationSignUpEmail = async (name, status) => {
+  try {
+    await transporter.sendMail({
+      to: process.env.ADMIN_EMAIL_ADDRESS, // TODO: fill in env
+      subject: "New org has signed up",
+      text: `New org has signed up\nOrg name: ${name}\nStatus: ${status}`,
+      html: `New org has signed up<br>Org name: ${name}<br>Status: ${status}`,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const sendUserSignUpEmail = async (id, name, email) => {
+  try {
+    await transporter.sendMail({
+      to: email,
+      subject: "Welcome to TSE Moneta!",
+      text: `Hi ${name},\n\nYou've been signed up for TSE Moneta! To create a password and complete the sign up process, please follow this link: TODO${id}\n\n-The Moneta Team`,
+      html: `Hi ${name},<br><br>You've been signed up for TSE Moneta! To create a password and complete the sign up process, please follow this link: TODO${id}<br><br>-The Moneta Team`,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
