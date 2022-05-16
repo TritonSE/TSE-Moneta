6b73147c1e64a0805bbf85a63d53a3b58c6afc3d
pratyush1718 <pratyush.chand@outlook.com>
2022-05-16 14:36:58 -0700

    updated

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ const organizationRoutes = require("./routes/organizationRoutes");
 const groupRoutes = require("./routes/groupRoutes");
 const typeRoutes = require("./routes/typesRoutes");
 
-// const type = require("./models/Types");
 
 connectDB();
 
